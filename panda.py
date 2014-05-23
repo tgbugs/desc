fa3b091600f0b8c5f3f5a60d6c5e3263284b0e61
Tom Gillespie <tgbugs@gmail.com>
2014-05-23 00:35:43 -0700

    added panda.py for playing with panda3d, which it seems can pull stuff to render directly from the net

---
+++ panda.py
@@ -0,0 +1,4 @@
+#so it looks like it is possible to stream stuff in using multifiles in pand3d
+#obviously in the long run we will probably want our own optimized renderer
+#but this is somehwere to start
+#install is a freeking mess

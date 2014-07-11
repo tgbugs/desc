9602e402d9c84bbe656ca933a9a0f74d65136e59
Tom Gillespie <tgbugs@gmail.com>
2014-07-10 19:59:02 -0700

    to use asyncio to get synchronous data one, possibly 'correct' way to do it is to write a coroutine that waits on a future and then returns the value we get back, because sometimes, eg during auth, cannot proceed until all the parts are in place (see: security policy)

--- client.py
+++ client.py
@@ -1,18 +1,27 @@
 import asyncio
 import random
 import pickle
+import ssl
 from time import sleep
 from IPython import embed
 from numpy.random import rand
+from defaults import CONNECTION_PORT, DATA_PORT
+
 
 def dumps(object):
     """ Special dumps that adds a double stop to make deserializing easier """
     return pickle.dumps(object)+b'.'
 
-class newConnectionProtocol(asyncio.Protocol):
+def run_for_time(loop,time):
+    """ use this to view responses inside embed """
+    loop.run_until_complete(asyncio.sleep(time))
+
+
+class dataProtocol(asyncio.Protocol):
     def connection_made(self, transport):
         transport.write(b'hello there')
         self.transport = transport
+
     def data_received(self, data):
         """ receive bam files that come back on request
             we can tag them with a request id if needs be
@@ -21,19 +30,61 @@ class newConnectionProtocol(asyncio.Protocol):
             arent cached
         """
         print("received",data)
+        self.data_callback(data)
     
     def connection_lost(self, exc):
         print('connection lost')
         asyncio.get_event_loop().close()
 
+    def set_data_callback(self,callback_function):
+        self.data_callback = callback_function
+"""
+authBytesPerBlock = 512
+authOpCodes = {
+    b'\x99\x99':'',
+    b'\x98\x98':'',
+    b'\x97\x97':'',
+}
+"""
+
+
+class newConnectionProtocol(asyncio.Protocol):
+    """ this is going to be done with fixed byte sizes known small headers """
+    def connection_made(self, transport):
+        self.transport = transport
+        self.transport.write(b'I promis I real client, plz dataz')
+        #send public key (ie the account we are looking for) #their password should unlock a local key which 
+
+    def data_received(self, data):
+        done = False
+        if done:
+            self.write_eof()
+        
+    def connection_lost(self, exc):
+        pass
 
 def main():
+    conContext = ssl.create_default_context(purpose=ssl.Purpose.SERVER_AUTH, cadata=None)  # TODO cadata should allow ONLY our self signed, severly annoying to develop...
+    dataContext = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+
     clientLoop = asyncio.get_event_loop()
-    coro_conClient = clientLoop.create_connection(newConnectionProtocol, '127.0.0.1', 55555, ssl=None)  # TODO ssl
+    coro_conClient = clientLoop.create_connection(newConnectionProtocol, '127.0.0.1', CONNECTION_PORT, ssl=None)  # TODO ssl
+    coro_dataClient = clientLoop.create_connection(dataProtocol, '127.0.0.1', DATA_PORT, ssl=None)  # TODO ssl
     #embed()
     #transport, protocol = yield from clientLoop.create_connection(newConnectionProtocol, '127.0.0.1', 55555, ssl=None)  # TODO ssl
     #reader, writer = yield from asyncio.open_connection('127.0.0.1', 55555, loop=clientLoop, ssl=None)
-    transport, protocol = clientLoop.run_until_complete(coro_conClient)
+
+    #transport, protocol = clientLoop.run_until_complete(coro_conClient)
+
+    #TODO ONE way to make this synchronous so to add a coroutine that only completes when it gets a response
+        #and then call run_until_complete on it :)
+    test = []
+    def myfunc(data):
+        test.append(data)
+
+
+    transport, protocol = clientLoop.run_until_complete(coro_dataClient)
+    protocol.set_data_callback(myfunc)
     transport.write(b'testing?')
     #writer.write('does this work?')
     for i in range(10):
@@ -42,12 +93,14 @@ def main():
         transport.write("testing post {} ?".format(i).encode())
 
     transport.write(dumps([random.random() for _ in range(100)]))
+    #a = yield from transport  # WHAT having this in here at all causes main to be skipped?!?!
+    #print('it works!',a)
     transport.write(dumps([random.random() for _ in range(100)]))
     transport.write(dumps([random.random() for _ in range(100)]))
     transport.write(dumps([random.random() for _ in range(100)]))
-    sleep(.001)
+    #sleep(.001)
     transport.write(dumps('numpy?!'))
-    sleep(.001)
+    #sleep(.001)
     transport.write(dumps(rand(100)))
     transport.write(dumps(rand(100)))
 
@@ -59,8 +112,11 @@ def main():
         #print('exiting...')
     #finally:
         #clientLoop.close()
-    clientLoop.close()
+    #clientLoop.close()
+
+#def main():
+    #print("WHAT IS GOING ON HERE")
 
 if __name__ == "__main__":
     main()
-        
+    print('ok')
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import asyncio
 import pickle
+import ssl
+
+from defaults import CONNECTION_PORT, DATA_PORT
 
 class requestManager(object):
     """ Server side class that listens for requests to render data to bam
@@ -24,34 +27,107 @@ class requestManager(object):
     def handleRequest(self):
         pass
 
-class connectionServerProtocol(asyncio.Protocol):
+class connectionServerProtocol(asyncio.Protocol):  # this is really the auth server protocol
     """ Define the protocol for handling basic connections
         should spin up client specific connections?
+
+        Turns out that this automatically spins up isolated
+        versions of itself for each connection, we just need
+        to make sure that we print the client info ahead of the
+        message so we know which connection we are getting data
+        about
     """
     def connection_made(self, transport):
-        peername = transport.get_extra_info('peername')
+        cert = transport.get_extra_info('peercert')
+        if not cert:
+            #make a cert, sign it ourselves so we know it came from us, and send it to the client
+            #use that cert id to get stored data if needs be
+            #this is NOT secure
+            pass
+        self.transport = transport
+        #for now we are just going to give clients peer certs and not worry about it
+
+        #client telling us this is a new user request
+
+        #client telling us this is an existing auth
+
+        #client telling us this is 
+
+        #not new user
+        #if not add it
+        #request the passphrase locked private key #this fails because people need to be able to change passwords >_<
+
+        #make some bytes
+        #encrypt those bytes with their public key
+        #send them the encrypted bytes
+        #wait to get the unencrypted bytes back
+        #check if they match #make sure this is done in constant time
+
+
+
         #print('connection from: %s'%peername)
+
+
+        #TODO I think we need to have a way to track 'sessions' or 'clients'
+            #NOT users, though that would still be useful if people are using
+            #the same system
+            #however, auth will all be done locally on the machine with the
+            #proper protections to prevent a malicious client fubar all
+        #since we all we really want is to be able to give a unique id its
+        #existing data...
+        #the ident and auth code probably needs to go here since establishing
+        #the transport is PRIOR to that, getting the SSLed channel comes first
+        #but we don't want application auth mixed up in that, so do it here
+        #XXX I suppose that we could do it all with ssl certs?
+            #ie: don't implement your own auth system tis hard
+
+        #if everything succeeds
+
+    def data_received(self, data):  # data is a bytes object
+        done = False
+        if data == b'I promis I real client, plz dataz':
+            self.transport.write(b'ok here dataz')
+        if done:
+            self.transport.write_eof()
+
+    def eof_received(self):
+        #clean up the connection and store stuff because the client has exited
+        print('got eof')
+
+    def send_auth_request(self):
+        self.transport.write()
+
+    def post_success(self):
+        #open up the firewall on the
+        pass
+    
+
+class dataServerProtocol(asyncio.Protocol):
+    #first of all ignore all packets received on 
+    #the port in question that have not passed auth
+    def connection_made(self, transport):
+        #check for session token
+        peername = transport.get_extra_info('peername')
         print("connection from:",peername)
+        self.pprefix = peername
         self.transport = transport
         self.__receiving__ = False
         self.__block__ = b''
 
-    def data_received(self, data):  # data is a bytes object
-        try:
-            print('data tail:',data[-10])
-        except IndexError:
-            print('data received: %s'%data)
-        print([t for t in self.process_data(data)])
-        #actually process the response
+    def data_received(self, data):
+        print(self.pprefix,[t for t in self.process_data(data)])
         self.transport.write(b'processed response')
+        #try:
+            #print(self.pprefix,'data tail:',data[-10])
+        #except IndexError:
+            #print(self.pprefix,'data received: %s'%data)
+        #print([t for t in self.process_data(data)])
+        #actually process the response
 
-    def process_data(self,data):
-        #are we already receiving a stream?
-            #new stream
-                #what type of stream is it?
-                #if the stream sends fixed sized requests just wait for bytes
-            #existing stream
-                #are we there yet?
+    def eof_received(self):
+        print('got eof')
+
+    def process_data(self,data):  # FIXME does this actually go here? or should it be in code that works directly on the transport object??!
         self.__splitStopPossible__ = False
         pickleStop = 0
         if not self.__receiving__:
@@ -70,7 +146,7 @@ class connectionServerProtocol(asyncio.Protocol):
                     pickleStop = None
                 self.__block__ += data[:pickleStop]
 
-        if pickleStop is None:  # this is confusing, -1 only occurs if we are receiving
+        if pickleStop is None:  # this is confusing, None only occurs if we are receiving and assumes all transport is done by pickle
             self.__receiving__ = 'pickle'  # FIXME why we set this every time >_<
             if data[-1] == b'.':
                 self.__splitStopPossible__ == True
@@ -82,7 +158,8 @@ class connectionServerProtocol(asyncio.Protocol):
                 block = self.__block__
                 self.__block__ = b''
                 self.__receiving__ = False
-                raise BaseException(block)
+                print(self.pprefix,block)
+                raise e
             self.__block__ = b''
             yield thing
             rest = data[pickleStop:]
@@ -91,24 +168,15 @@ class connectionServerProtocol(asyncio.Protocol):
                 yield from self.process_data(rest[1:])  # FIXME this can triggers recursion error on too many ..
             self.__receiving__ = False
 
-                ##look for pickle end
-            #else:
-                #pass
-                #look for newline or rather, just ignore it
-
-    #def dispatch_block(self):
-        #processBlock(self.transport,self.__block__)  # this should probably yield a future or something to keep it async
-
-    def eof_received(self):
-        #clean up the connection and store stuff because the client has exited
-        print('got eof')
-        pass
-    
 
 def main():
+    conContext = ssl.create_default_context(purpose=ssl.Purpose.SERVER_AUTH, cafile=None)
+    dataContext = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
     serverLoop = asyncio.get_event_loop()
-    coro_conServer = serverLoop.create_server(connectionServerProtocol, '127.0.0.1', 55555, ssl=None)  # TODO ssl
+    coro_conServer = serverLoop.create_server(connectionServerProtocol, '127.0.0.1', CONNECTION_PORT, ssl=None)  # TODO ssl
+    coro_dataServer = serverLoop.create_server(dataServerProtocol, '127.0.0.1', DATA_PORT, ssl=None)  # TODO ssl and this can be another box
     server = serverLoop.run_until_complete(coro_conServer)
+    server = serverLoop.run_until_complete(coro_dataServer)
     try:
         serverLoop.run_forever()
     except KeyboardInterrupt:

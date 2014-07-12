05b0d83086b49228305118a7996dc2167a85f5b8
Tom Gillespie <tgbugs@gmail.com>
2014-07-11 19:35:57 -0700

    forgot to add requests.py

---
+++ request.py
@@ -0,0 +1,43 @@
+
+class Request:  # testing only since this will need to be in its own module to keep python happy
+    """ the serializable object used for transporting the requested data
+        (gleaned from UI input) and sending it to the server to retrieve
+        this thing needs to have an injective mapping to the data
+        
+        fortunately all of the data used to generate the request is
+        pulled from the datastore itself so that should simplifying things
+
+        argh, since we probably shouldn't / can't dump whole collision trees
+        to bam we *may* need to send the uuid data in the serialization of points
+    """
+    def __init__(self,request_type:'[raw (sql or something?), data view,'
+                 ' type/knowledge view, any relationship view??]',
+                 type_:'uuid', properties:'[0..n]', constraints:'a set of'
+                 ' constraints on properties (cache/eager load the rest too so you change selectors locally '):
+        #should properties be generated on the fly as an enum for the interface? no, they will bugger the hash
+        self.request_type = request_type
+        self.type_ = type_   #FIXME if the request type is for know then here we should expect a list... HRM
+        self.properties = properties
+        self.hash_ = None
+        def __hash__(self):
+            return self.hash_
+
+def main():
+    from enum import Enum
+    from IPython import embed
+
+    class REQUEST_TYPES(Enum):
+        # for some reason I think we really do not want to hard code this since request types seem likely to ... grow?
+        # actually the function API seems pretty cool...
+        Data = 0
+        Type = 1
+        Rel = 2
+        Cookies = 3
+
+    print([hash(t) for t in REQUEST_TYPES])  # hash is not consistent across sessions, to be fair that is probably correct given the definition of hash
+    print([hash(t) for t in range(4)])
+
+    
+
+if __name__ == '__main__':
+    main()

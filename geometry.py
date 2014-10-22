081514ac7ee1f524defd2581cb3bbfe60e26ec1f
Tom Gillespie <tgbugs@gmail.com>
2014-10-22 16:00:40 -0700

    trying to work on geometry but somewhere in here the server seems to have broken

--- geometry.py
+++ geometry.py
@@ -3,7 +3,33 @@
     to handle the geometry and collision creation from data
 """
 
-def makeBam(positions, geometry, colors):
+from panda3d.core import GeomVertexFormat, GeomVertexData
+
+#geomTypes
+from panda3d.core import GeomTristrips, GeomTriangles
+from panda3d.core import GeomLinestrips, GeomLines
+from panda3d.core import GeomPoints
+
+
+class geometryData:
+    def __init__(self, positions, geometry, colors, uuids, bounds = None):
+        pass
+
+
+def convert_to_geometry(something):
+    return property_formatted_geometry
+
+def makeGeom(positions, geometry, colors):
+    #geometry shall be?
+        #a list of?
+        #lists of (position, geometry, color) tuples
+        # one of the 5 geom types listed above in imports
+            # if it is this then the length of the geometry should be the tipoff?
+        #Geoms?
+        #
+
+
+
     #for each position:
         #if we want something more than points:
             #ASSUME THAT ALL GEOMETRY HAS ALREADY BEEN ZEROED!
@@ -14,6 +40,10 @@ def makeBam(positions, geometry, colors):
 
     #flatten
 
+    return geom
+
+def makeBam(geom):
+    geom.__reduce__().[1][-1]  # look at the product of this reduce to see why 1,-1
     return bam
 
 def makeCol(positions, uuids, bounds):

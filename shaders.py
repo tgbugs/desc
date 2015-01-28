625e71ebc5919e95afd0b93df560f9af1cd0da8a
Tom Gillespie <tgbugs@gmail.com>
2015-01-28 01:49:34 -0800

    have figured out how to load the 3d texture directly from numpy (woo fast) now just have to figure out what the indexing scheme actually is relatvie to xyz and get the shader working

--- shaders.py
+++ shaders.py
@@ -2,25 +2,157 @@
 #also panda documentation
 #to do 4d stuff we just set up a bunch of 3d textures and swap them and keep the same shader
 from panda3d.core import Shader
+from panda3d.core import Texture, TextureStage, TexGenAttrib
+from panda3d.core import PNMImage
+from panda3d.core import GeomNode
+from panda3d.core import GeomTriangles, GeomTristrips, GeomTrifans
 
-from panda3d.core import Texture
+from panda3d.core import Geom, GeomVertexWriter, GeomVertexFormat, GeomVertexData
+#from panda3d.core import loader
 
-from panda3d.core import loader
+import numpy as np
+
+from ipython import embed
+
+def make_cube(x, y, z):  # FIXME make prism
+    """ make x, y, z sized cube (ints pls) """
+    colors = [[1,1,1,1] for i in range(8)]
+    #colors[0] = np.array((1,1,1,1))
+    #colors[1] = np.array((1,0,0,0))
+    #colors[2] = np.array((0,1,0,0))
+    #colors[5] = np.array((0,0,1,0))
+    points = (
+        (0,0,0),
+        (0,0,z),
+        (0,y,0),
+        (0,y,z),
+        (x,0,0),
+        (x,0,z),
+        (x,y,0),
+        (x,y,z),
+    )
+    order = [0, 5, 1, 7, 3, 2, 1, 0, 5, 4, 7, 6, 2, 4, 0]  # perfect for clockwise
+    #order = [2, 6, 3, 7, 5, 6, 4, 2, 0, 3, 1, 4, 0, 4]
+    #order.reverse()
+    #order = [4, 3, 7, 8, 5, 3, 1, 4, 2, 7, 6, 5, 2, 1]
 
 
-tex = Texture.setup3dTexture(sx, sy, sz, component_type, format_)  #all ints
-# TODO how to read the matrix in?!
+    fmt = GeomVertexFormat.getV3c4()
+    vertexData = GeomVertexData('points', fmt, Geom.UHStatic)
+    verts = GeomVertexWriter(vertexData, 'vertex')
+    color = GeomVertexWriter(vertexData, 'color')
 
-nodePath.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldPosition)
-nodePath.setTexGen(TextureStage.getDefault(), 0, 0, 0)
-nodePath.setTexture(tex)
+    for p,c in zip(points,colors):
+        verts.addData3f(*p)
+        color.addData4f(*c)
 
-myShader = Shader.load(Shader.SL_GLSL, "my_vert.glsl", "my_frag.glsl", "my_geom.glsl")
+    targetTris = GeomTristrips(Geom.UHStatic)
+    #targetTris.addConsecutiveVertices(0,8)
+    for i in order:
+        targetTris.addVertex(i)#-1)
+    targetTris.closePrimitive()
 
-nodePath.set_shader(myShader)
-nodePath.set_shader_input("my_param",(1,1,1))
-
+    target = Geom(vertexData)
+    target.addPrimitive(targetTris)
+    return target
 # apparently the sizes need to be powers of 2??? but maybe we can just map the relevant coords 1:1
 
 # TODO for volume rendering we will hopefully use the alpha value?
 # not clear exactly how to load in a full 3d matrix...
+
+def make_texture3d(array):
+    """ assumes x y z ordering for slicing """
+    tex = Texture()
+    xl,yl,zl = array.shape
+
+    tex.setup3dTexture(xl, yl, zl, Texture.TUnsignedByte, Texture.FLuminanceAlphamask)  #all ints
+    mv = memoryview(tex.modifyRamImage())
+    asdf = np.asarray(mv)
+    #embed()
+    #asdf[1] = 255
+    #asdf[0] = 255
+    #asdf[:256:2] = 255
+    # [::2] indexes channel 0, so it seems that the channels are interspersed RGBA...RGBA style
+    # ^ the above seems incorrect... the thing should be a 3d matrix... yet somehow?
+
+    asdf[::2] = 255
+    #asdf[1::2] = 255  #doesn't seem to do anything
+    #asdf[::2] = array.flatten()  # FIXME surely there is a faster way?
+    #asdf[array.size::2] = 0
+    #for i in range(128):
+        #asdf[i::256] = 0
+
+    #asdf[1::2] = 1
+    #embed()
+    """
+    for i in range(zl):
+        p = PNMImage(xl, yl, 1)
+
+        for x in range(xl):  # FIXME SLOW AS BALLS
+            for y in range(yl):
+                p.setChannelVal(x, y, 0, array[x, y, i])
+
+        tex.load(p, i, 0)
+    #"""
+
+    return tex, None
+
+
+###
+#   Tests
+###
+
+def main():
+    from direct.showbase.ShowBase import ShowBase
+    from util import startup_data, exit_cleanup, ui_text, console, frame_rate
+    from ui import CameraControl, Axis3d, Grid3d
+    from keys import AcceptKeys, callbacks
+
+    base = ShowBase()
+    base.disableMouse()
+    base.setBackgroundColor(0,0,0)
+    startup_data()
+    frame_rate()
+    uit = ui_text()
+    con = console()
+    ec = exit_cleanup()
+    cc = CameraControl()
+    ax = Axis3d()
+    gd = Grid3d()
+    ac = AcceptKeys()
+
+
+    ###
+    # XXX REAL CODE HERE
+    ###
+
+    array = np.random.randint(0,255,(256,256,256))
+    tex, memarray = make_texture3d(array)
+    tex2 = Texture()
+
+
+    # TODO how to read the matrix in?!
+
+    geomNode = GeomNode('shader_test')
+    geomNode.addGeom(make_cube(256,256,256))
+    nodePath = render.attachNewNode(geomNode)
+    nodePath.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldPosition)
+    nodePath.setTexture(tex)
+    #embed()
+    #nodePath.setTexGen(TextureStage.getDefault(), 0, 0, 0)  #bug?
+
+    """
+    myShader = Shader.load(Shader.SL_GLSL, "my_vert.glsl", "my_frag.glsl")#, "my_geom.glsl")
+
+    # wow, this actually... turns the box black or something, probably shound't attach the texture to the nodepath if we do it this way
+    nodePath.set_shader(myShader)
+    #nodePath.set_shader_input("my_param",(1,1,1))
+    nodePath.set_shader_input("tex",tex2)
+    nodePath.set_shader_input("volume_tex",tex)
+    nodePath.set_shader_input("stepsize",.5)
+    #"""
+
+    run()
+
+if __name__ == '__main__':
+    main()

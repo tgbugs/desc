9d220afff732a1ef2b5a7afbe42cbbbe92500488
Tom Gillespie <tgbugs@gmail.com>
2017-08-07 16:35:24 -0700

    prof improved profile_me to accept sort kwarg

--- prof.py
+++ prof.py
@@ -3,7 +3,7 @@
     to delimit the code you want to profile
 """
 import cProfile, pstats, io
-from functools import wraps
+from functools import wraps, partial
 from .util.ipython import embed
 
 class Prof:
@@ -23,9 +23,22 @@ class Prof:
 
 #decorator to do this
 
-def profile_me(function):
+def doublewrap(decorator_function):
+    @wraps(decorator_function)
+    def new_dec(*args, **kwargs):
+        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
+            return decorator_function(args[0])
+        else:
+            def actual_decorator(function):
+                return decorator_function(function, *args, **kwargs)
+            return actual_decorator
+
+    return new_dec
+
+@doublewrap
+def profile_me(function, sort='tottime'):
     @wraps(function)
-    def wrapped(*args,**kwargs):
+    def wrapped(*args, **kwargs):
         if not globals()['profile_me_already_run'+function.__qualname__]:
             globals()['profile_me_already_run'+function.__qualname__] = True
             pr = cProfile.Profile()
@@ -34,7 +47,7 @@ def profile_me(function):
             out = function(*args, **kwargs)
             globals()['profile_me_already_run'+function.__qualname__] = False  # recursion complete
             pr.disable()
-            ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
+            ps = pstats.Stats(pr, stream=s).sort_stats(sort)
             ps.print_stats()
             print(function.__name__,s.getvalue())
             return out
